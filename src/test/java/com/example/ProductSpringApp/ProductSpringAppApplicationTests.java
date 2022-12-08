0317ff0507c82aa0a98e1199118838a211d84c27
melvinsibichen <melvinsibichen1020@gmail.com>
2022-12-08 20:07:21 +0530

    first

---
+++ src/test/java/com/example/ProductSpringApp/ProductSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProductSpringApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
